49ec5df6b32b0d06b8ec04ac2a8f494a2af6a9c9
Гульгина <114882919+TuxvGylgina@users.noreply.github.com>
2022-11-24 16:21:39 +0300

    Практика 2.2.3.а

--- App.scala
+++ App.scala
@@ -6,7 +6,12 @@ package org.example
 object App {
   
   def main(args : Array[String]) {
-    println( "Hello, Scala!" )
+    var starVarString = "Hello, Scala!"
+    println(starVarString.reverse)
+    println(starVarString.toLowerCase)
+    println(starVarString.init)
+    println(starVarString + " and goodbye python!")
+
   }
 
 }
